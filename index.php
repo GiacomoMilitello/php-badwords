f31467ab2fd941ec92a0ab464b28910de65733b7
GiacomoMilitello <g.militello95@gmail.com>
2024-02-23 18:12:17 +0100

    starting

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    $titolo = "intro php"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+    
+    <?php echo $titolo ?>
+
+</body>
+</html>
